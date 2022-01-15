da6074db6e7219b1e6184f2bf5368409beaa0000
Developer Xabdaz <developer@xabdaz.tech>
2022-01-15 16:47:27 +0700

    create base viewController

---
+++ Core/Base/View/EXViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EXViewController.swift
+//  otoklixSubmition
+//
+//  Created by Developer Xabdaz on 15/01/22.
+//
+
+import UIKit
+
+open class EXViewController: UIViewController {
+    
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil ?? "\(Self.self)", bundle: nibBundleOrNil ?? Bundle.main)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    deinit {
+        self.onFinishCoordinator()
+    }
+
+    open func onFinishCoordinator() {
+        print("please implemented to destroy coordinator")
+    }
+}
