6d32d9e76a43f5504cdfd23c2d1e8f59e5795ec8
Developer Xabdaz <developer@xabdaz.tech>
2022-01-15 17:26:59 +0700

    add exteension container

---
+++ Core/Extension/Containter+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Containter+Extension.swift
+//  otoklixSubmition
+//
+//  Created by Developer Xabdaz on 15/01/22.
+//
+
+import Foundation
+import Swinject
+
+extension Container {
+    func registerDependencies() {
+    }
+}
