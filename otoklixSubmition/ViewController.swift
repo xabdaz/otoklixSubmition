1a3a7768df4ff72689d29efd049eadb1cfd3de85
Developer Xabdaz <developer@xabdaz.tech>
2022-01-15 16:02:05 +0700

    Initial Commit

---
+++ otoklixSubmition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  otoklixSubmition
+//
+//  Created by Developer Xabdaz on 15/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
